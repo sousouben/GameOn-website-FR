217c320f104b5af66060ec94b75b5b853d12ad91
sousouben <s.mouatakide@laposte.net>
2022-04-28 10:30:40 +0200

    fonction initialize

--- js/modal.js
+++ js/modal.js
@@ -45,6 +45,7 @@ modalClose.addEventListener("click", closeModal); //pour fermer la modal au clic
 // launch modal form
 function launchModal() {
   modalBg.style.display = "block";
+  initializeFields();
 }
 
 //launch close modal
@@ -55,17 +56,20 @@ function closeModal() {
 // fonction de vérification du champs prénom
 
 function checkOutFirstName() {
-  if (!firstName.value) {//si champs vide erreur
+  if (!firstName.value) {
+    //si champs vide erreur
     //si le champs est vide
     firstNameError.innerHTML = "Veuillez renseigner votre prénom";
     firstNameError.style.display = "block";
     return false;
-  } else if (firstName.value.length < 2) {// si il y a moins de 2 charactères donc erreur
+  } else if (firstName.value.length < 2) {
+    // si il y a moins de 2 charactères donc erreur
     firstNameError.innerHTML =
       "Veuillez entrer 2 caractères ou plus pour votre prénom";
     firstNameError.style.display = "block";
     return false;
-  } else {//si tous est bon pas de message d'erreur
+  } else {
+    //si tous est bon pas de message d'erreur
     firstNameError.style.display = "none";
     return true;
   }
@@ -74,16 +78,19 @@ function checkOutFirstName() {
 // fonction de vérification du champs nom
 
 function checkOutLastName() {
-  if (!lastName.value) {//si champs vide erreur
+  if (!lastName.value) {
+    //si champs vide erreur
     lastNameError.innerHTML = "Veuillez renseigner votre nom";
     lastNameError.style.display = "block";
     return false;
-  } else if (lastName.value.length < 2) {//si il y a moins de 2 charactères donc erreur
+  } else if (lastName.value.length < 2) {
+    //si il y a moins de 2 charactères donc erreur
     lastNameError.innerHTML =
       "Veuillez entrer 2 caractères ou plus pour votre nom";
     lastNameError.style.display = "block";
     return false;
-  } else {//si tous est bon pas de message d'erreur
+  } else {
+    //si tous est bon pas de message d'erreur
     lastNameError.style.display = "none";
     return true;
   }
@@ -95,16 +102,19 @@ let regexEmail =
 
 // fonction de vérification du champs email
 function checkOutEmail() {
-  if (!email.value) {// si le champs est vide message d'erreur
+  if (!email.value) {
+    // si le champs est vide message d'erreur
     emailError.innerHTML = "Veuillez renseigner votre adresse email";
     emailError.style.display = "block";
     return false;
-  } else if (regexEmail.exec(email.value) == null) {//si se n'est pas conforme au regex email donc pas valide
+  } else if (regexEmail.exec(email.value) == null) {
+    //si se n'est pas conforme au regex email donc pas valide
     emailError.innerHTML =
       "Entrez une adresse valide. Exemple : contact@nom.com";
     emailError.style.display = "block";
     return false;
-  } else {//si tout est respecté pas de message d'erreur
+  } else {
+    //si tout est respecté pas de message d'erreur
     emailError.style.display = "none";
     return true;
   }
@@ -114,9 +124,10 @@ function checkOutEmail() {
 function checkOutBirth() {
   //récupération de la date
   let dateBirth = birthdate.value;
-  console.log(dateBirth.split("-")[0]);//methode qui récupère l'année
+  console.log(dateBirth.split("-")[0]); //methode qui récupère l'année
 
-  if (!dateBirth) {//si la date n'est pas renseigné champs vide
+  if (!dateBirth) {
+    //si la date n'est pas renseigné champs vide
     ageError.innerHTML = "Veuillez renseigner votre date de naissance";
     ageError.style.display = "block";
     return false;
@@ -124,11 +135,13 @@ function checkOutBirth() {
     let birthYear = dateBirth.split("-")[0];
     let currentYear = new Date().getFullYear(); // variable qui récupère la date actuelle
     let age = currentYear - birthYear;
-    if (age <= 17) {//si l'age est inferieur ou égale à 17 ans donc erreur car il faut avoir 18 ans
+    if (age <= 17) {
+      //si l'age est inferieur ou égale à 17 ans donc erreur car il faut avoir 18 ans
       ageError.innerHTML = "votre date de naissance n'est pas valide!";
       ageError.style.display = "block";
       return false;
-    } else {//si tout est bon on enlève le message d'erreur
+    } else {
+      //si tout est bon on enlève le message d'erreur
       ageError.style.display = "none";
       return true;
     }
@@ -137,24 +150,28 @@ function checkOutBirth() {
 
 // fonction de vérification du champs quantity
 function checkOutQuant() {
-  if (!participation.value) {// si le champs est vide message d'erreur
+  if (!participation.value) {
+    // si le champs est vide message d'erreur
     participationError.innerHTML =
       "Merci de saisir un nombre de participations";
     participationError.style.display = "block";
     return false;
-  } else if (participation.value > 99) {// si le nombre est suppérieur à 99 donc erreur
+  } else if (participation.value > 99) {
+    // si le nombre est suppérieur à 99 donc erreur
     participationError.innerHTML =
       "Vous avez atteint un nombre maximal d'entrées.";
     participationError.style.display = "block";
     return false;
-  } else {//si tout est bon on enlève le message d'erreur
+  } else {
+    //si tout est bon on enlève le message d'erreur
     participationError.style.display = "none";
     return true;
   }
 }
 
 //tableau des boutons radios
-let locationArray = [//création d'un variable qui récupère tout les boutons radios du DOM
+let locationArray = [
+  //création d'un variable qui récupère tout les boutons radios du DOM
   document.querySelector("#location1"),
   document.querySelector("#location2"),
   document.querySelector("#location3"),
@@ -178,7 +195,8 @@ function checkOutCity() {
       "veuillez renseinger une ville pour pouvoir participer";
     cityError.style.display = "block";
     return false;
-  } else {//si un bouton est coché plus d'erreur
+  } else {
+    //si un bouton est coché plus d'erreur
     cityError.style.display = "none";
     return true;
   }
@@ -186,12 +204,14 @@ function checkOutCity() {
 
 //fonction de vérification de la CGU (Conditions générales d'utilisation) cochée ou décochée
 function checkedCgu() {
-  if (!cgU.checked) {//si le bouton n'est pas coché alors erreur
+  if (!cgU.checked) {
+    //si le bouton n'est pas coché alors erreur
     errorCgu.innerHTML =
       "Veuillez accepter les conditions générales d'utilisation";
     errorCgu.style.display = "block";
     return false;
-  } else {//s'il est coché plus d'erreur
+  } else {
+    //s'il est coché plus d'erreur
     errorCgu.style.display = "none";
     return true;
   }
@@ -227,8 +247,19 @@ function validForm(e) {
     modalBg.style.display = "none";
     bground.style.display = "block"; // le fond d'écran s'affiche en background-color: rgba(26, 39, 156, 0.4);
     modalGood(); //on appel la fonction de validation
-    return true; // validation du formulaire
+    initializeFields();
   }
+}
+
+//initialisation de tous les champs du formulaire
+function initializeFields() {
+  firstName.value = null;
+  lastName.value = null;
+  email.value = null;
+  birthdate.value = null;
+  participation.value = null;
+  city.checked = false;
+  cgU.checked = false;  
 }
 
 // fermeture de la modal de remerciement
